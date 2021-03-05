b05e117d4242083367cfaa8e22486314d10c2d4a
Tauschrausch-Team <77614021+Tauschrausch-Team@users.noreply.github.com>
2021-03-05 16:45:05 +0100

    Update philip.js

--- pages/philip.js
+++ pages/philip.js
@@ -18,6 +18,7 @@ module.exports = async function(pkt,abinote) {
 	var html = new Template('./html/philip.html',{
 		// Hier können Daten an das HTML-Dokument übergeben werden.
 		punkte: pkt
+		abinote: abinote
 	});
 	return await html.parse();
 }
